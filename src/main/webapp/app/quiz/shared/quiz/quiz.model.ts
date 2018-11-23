05faf770265f77b8406210e7683435ba0d0bbbae
Julien Margarido <julien.margarido@ext.saint-gobain.com>
2018-11-23 17:30:06 +0100

    started get randomized quizes service method

--- src/main/webapp/app/quiz/shared/quiz/quiz.model.ts
+++ src/main/webapp/app/quiz/shared/quiz/quiz.model.ts
@@ -2,8 +2,8 @@ import { Question } from 'app/quiz/shared/question/question.model';
 import { QuizConfig } from './quiz-config.model';
 
 export interface Quiz {
-  id: string;
-  name: string;
+  id?: string;
+  name?: string;
   description?: string;
   config?: QuizConfig;
   questions: Question[];
--- src/main/webapp/app/quiz/shared/services/quiz.service.ts
+++ src/main/webapp/app/quiz/shared/services/quiz.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { QUIZES_URL } from 'app/app.constants';
+import { CategoriesInfo, Category } from 'app/quiz/shared/categories-info/categories-info.model';
 import { Quiz } from 'app/quiz/shared/quiz/quiz.model';
-import { NamedObject } from 'app/shared/models/named-object.model';
-import { CategoriesInfo } from 'app/quiz/shared/categories-info/categories-info.model';
-import { Category } from 'app/quiz/shared/categories-info/categories-info.model';
 import { FileObject } from 'app/shared/models/file-object.model';
 
 @Injectable()
 export class QuizService {
+  INFO_FILENAME = 'categories.json';
+
   constructor(private http: HttpClient) {}
 
-  get(url: string): Promise<Quiz> {
+  getQuizByUrl(url: string): Promise<Quiz> {
     return this.http.get<Quiz>(url).toPromise();
   }
 
@@ -18,63 +19,58 @@ export class QuizService {
     return this.getQuizByNames(category.folder, fileName);
   }
 
+  getCategoriesInfo(): Promise<CategoriesInfo> {
+    const jsonFile = QUIZES_URL + this.INFO_FILENAME;
+    return this.http.get<CategoriesInfo>(jsonFile).toPromise();
+  }
+
   getQuizByNames(categoryName: string, fileName: string): Promise<Quiz> {
-    const url = 'content/json/' + categoryName + '/' + fileName;
+    const url = QUIZES_URL + categoryName + '/' + fileName;
     return this.http.get<Quiz>(url).toPromise();
   }
 
   /** TODO and FIXME: use DB !! */
-  getQuizesInfoByDirectory(category: Category): Promise<FileObject[]> {
-    const dirUrl = 'content/json/' + category.folder + '/';
-    return this.http
-      .get<string[]>(dirUrl)
-      .toPromise()
-      .then(jsons => {
-        if (!jsons || jsons.length === 0) {
-          console.error('Cannot list files in : ' + dirUrl);
-          jsons = ['n1_' + category.folder + '.json']; // TODO: load other files
-        }
-
-        return Promise.all(
-          jsons.map(json => {
-            return this.get(dirUrl + json).then(quiz => {
-              return { id: quiz.id, name: quiz.name, filename: json } as FileObject;
-            });
-          })
-        );
-      });
-  }
-
-  /** TODO and FIXME: use DB !! */
   getQuizesInfoByCategory(category: Category): Promise<FileObject[]> {
-    const dirUrl = 'content/json/' + category.folder + '/';
+    const dirUrl = QUIZES_URL + category.folder + '/';
     return Promise.all(
       category.files.map(json => {
-        return this.get(dirUrl + json).then(quiz => {
+        return this.getQuizByUrl(dirUrl + json).then(quiz => {
           return { id: quiz.id, name: quiz.name, filename: json } as FileObject;
         });
       })
     );
   }
 
-  getDefaultCategory(): Promise<Category> {
-    return this.getCategoriesInfo()[0];
-  }
-
-  getCategoriesInfo(): Promise<CategoriesInfo> {
-    const jsonFile = 'content/json/categories.json';
-    return this.http.get<CategoriesInfo>(jsonFile).toPromise();
+  getQuizesByCategory(category: Category): Promise<Quiz[]> {
+    const dirUrl = QUIZES_URL + category.folder + '/';
+    return Promise.all(category.files.map(json => this.getQuizByUrl(dirUrl + json))) as Promise<Quiz[]>;
   }
 
   getCategoriesNameWithQuizes(): Promise<Map<Category, FileObject[]>> {
-    return this.getCategoriesInfo().then(async (info: CategoriesInfo) => {
+    return this.getCategoriesInfo().then((info: CategoriesInfo) => {
       const resMap = new Map<Category, FileObject[]>();
 
-      const putQuizesIntoMap = function(cat: Category, $this) {
-        $this.getQuizesInfoByCategory(cat).then(quizes => resMap.set(cat, quizes));
+      const putQuizesIntoMap = (cat: Category): Promise<any> => {
+        return this.getQuizesInfoByCategory(cat).then(quizes => resMap.set(cat, quizes));
       };
 
-      return await Promise.all(info.categories.map(cat => Promise.resolve(putQuizesIntoMap(cat, this)))).then(() => resMap);
+      return Promise.all(info.categories.map(cat => Promise.resolve(putQuizesIntoMap(cat)))).then(() => resMap);
+    });
+  }
+
+  getRandomizedQuiz(): Promise<Quiz> {
+    return this.getCategoriesInfo().then((info: CategoriesInfo) => {
+      const allQuizes: Quiz[] = [];
+
+      const collectQuizes = (cat: Category): Promise<any> => {
+        return this.getQuizesByCategory(cat).then(quizes => allQuizes.push(...quizes));
+      };
+
+      return Promise.all(info.categories.map(cat => Promise.resolve(collectQuizes(cat)))).then(() => {
+        // debugger;
+        return allQuizes[0];
+        // return { questions: [] } as Quiz;
+      });
     });
   }
 }
